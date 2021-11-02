21e77bbca39a9b49d03962cee64de5e6493c2815
matteofab10 <matteofabio10@gmail.com>
2021-11-02 09:50:24 +0100

    aggiunta esercizio pari dispari post correzione in classe

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// esercizio palindromo
+
+
 const str = prompt('Inserisci una parola');
 
 const isPalindrome = (inStr) => {
@@ -10,3 +13,36 @@ const isPalindrome = (inStr) => {
 }
 
 console.log(isPalindrome(str));
+
+
+// esercizio pari e dispari
+
+const pariDispari = prompt('inserisci pari o dispari');
+const numeroUtente = prompt('inserisci un numero da 1 a 9');
+const numeroPc = getRandomInt(1,9);
+console.log(numeroPc);
+const somma = numeroPc + numeroUtente
+console.log(somma);
+const risultato = checkPariDispari(somma)
+console.log(risultato);
+
+if (risultato === pariDispari){
+  console.log('ha vinto il giocatore');
+} else {
+  console.log('ha vinto il pc');
+}
+
+
+function checkPariDispari(numero){
+  if(numero % 2) {
+    return 'dispari';
+}
+    return 'pari';
+}
+
+
+
+
+function getRandomInt(min,max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
